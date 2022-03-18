b31eddb17672882a6d22ad54269c910bb4507a70
KaekoSakamoto <SKAEKO@jp.ibm.com>
2022-03-18 23:50:08 +0900

    Create SampleCode_DevC.py

---
+++ Practice/SampleCode_DevC.py
@@ -0,0 +1,18 @@
+
+
+tab="\t"*4
+this_mod_name="[DevC]"
+
+
+def initial():
+    print(tab,this_mod_name, this_mod_name, "が呼び出されました")
+    ret_code = __end_edge()
+    print(tab,this_mod_name, "が完了しました")
+    return ret_code
+
+def __end_edge():
+    ret_code = "999"
+    print(tab,this_mod_name, "処理終了。( RC=",ret_code,")")    
+
+    return ret_code
+
